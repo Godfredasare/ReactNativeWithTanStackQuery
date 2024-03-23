e68179966d87f09f45625d99e3056affb8c13d4a
Godfredasare <godasare8346@gmail.com>
2024-03-23 16:35:09 +0000

    first commit

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Image, StyleSheet } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { Text, View } from '@/components/Themed';
+
+import { Stack, useLocalSearchParams } from 'expo-router'
+import { useQuery } from '@tanstack/react-query'
+import { fetchMoviesDetails } from './service/movies-api'
 
 const MoviesDetails = () => {
-  const {id} = useLocalSearchParams()
+  const { id } = useLocalSearchParams()
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['movies', id],
+    queryFn: () => fetchMoviesDetails(id)
+  })
+
+
+
+
+
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+  if (error) {
+    return <Text>Error fetching movies</Text>
+  }
+
   return (
     <View>
-      <Text>MoviesDetails: {id}</Text>
+      <Stack.Screen options={{title: data.title}} />
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500/` + data.backdrop_path }}
+        style={{
+          width: '100%',
+          height: 300
+        }}
+      />
+      <View style={{padding: 10}}>
+      <Text style={{fontSize: 25, fontWeight: '500',marginVertical:10}}>{data.title}</Text>
+      <Text style={{fontSize: 16}}>{data.overview}</Text>
+
+      </View>
     </View>
   )
 }
--- app/service/movies-api.tsx
+++ app/service/movies-api.tsx
@@ -1,3 +1,12 @@
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2MxZGQ1YjNkNzViNTVmNGRiMzU5OWJhYWNkNDNhZCIsInN1YiI6IjYxZmZhYTMxMWZkMzZmMjI0YTEyMzBkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JJlV0CpnaHI9v_dfHQ7gkEKmfOyglq_18pf5qUHkxvc'
+//     }
+// }
+
 export const fetcRatedMovies = async () => {
     const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
     const options = {
@@ -6,7 +15,7 @@ export const fetcRatedMovies = async () => {
             accept: 'application/json',
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2MxZGQ1YjNkNzViNTVmNGRiMzU5OWJhYWNkNDNhZCIsInN1YiI6IjYxZmZhYTMxMWZkMzZmMjI0YTEyMzBkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JJlV0CpnaHI9v_dfHQ7gkEKmfOyglq_18pf5qUHkxvc'
         }
-    };
+    }
 
     const res = await fetch(url, options)
     if (!res.ok) {
@@ -16,5 +25,25 @@ export const fetcRatedMovies = async () => {
     const json = await res.json()
     return json.results
 
+
+}
+
+export const fetchMoviesDetails = async (id: number) => {
+
+    const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2MxZGQ1YjNkNzViNTVmNGRiMzU5OWJhYWNkNDNhZCIsInN1YiI6IjYxZmZhYTMxMWZkMzZmMjI0YTEyMzBkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JJlV0CpnaHI9v_dfHQ7gkEKmfOyglq_18pf5qUHkxvc'
+        }
+    }
+    const res = await fetch(url, options)
+    if (!res.ok) {
+        throw new Error("failed to ffetch movies");
+
+    }
+    const json = await res.json()
+    return json
 
 }
